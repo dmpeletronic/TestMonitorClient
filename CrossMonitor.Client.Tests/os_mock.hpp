9f6bc3f31e8f46acbd79811d2ddbefac2023c9a6
Danilo Pagano <dmpeletronic@gmail.com>
2019-02-16 18:55:10 -0200

    Fix unit test compilation

--- CrossMonitor.Client.Tests/os_mock.hpp
+++ CrossMonitor.Client.Tests/os_mock.hpp
@@ -8,6 +8,10 @@ namespace os {
 void set_process_count(unsigned int n);
 void set_cpu_use_percent(float percent);
 void set_memory_use_percent(float percent);
+void set_used_memory(unsigned long long used_memory);
+void set_total_memory(unsigned long long total_memory);
+void set_total_disk_read(unsigned long long total_disk_read);
+void set_total_disk_write(unsigned long long total_disk_write);
 
 } //namespace os
 } //namespace client
